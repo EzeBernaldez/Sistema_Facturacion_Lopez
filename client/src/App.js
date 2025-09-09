153c7ea6329ac296478bd3d3fa5a7db16e539680
EzeBernaldez <ezebernaldez@gmail.com>
2025-09-08 23:05:27 -0300

    react-router-dom installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import Home from './pages/Home';
 import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
@@ -12,9 +10,7 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/" element={<Login />} />
           </Routes>
         </div>
       </Router>
